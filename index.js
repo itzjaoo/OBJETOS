3ae88e6e2e7bc320bbed2b13550c44ab9deb5a5c
mailorcanto <mailorcanto@gmail.com>
2023-05-10 15:58:26 -0300

    subindo exercicios

---
+++ index.js
@@ -0,0 +1,141 @@
+// const nome = "jose"
+// const idade = 25
+// const endereco = " rua do bobo, numero 0"
+// const senha ="senha123"
+
+// const endereco = {
+//     rua:"rua do bobo",
+//     numero:0,
+//     cidade:"sao leopoldo",
+//     estado:"rio grande do sul"
+// }
+
+// const usuario = {
+//     nome:"mariana",
+//     idade:16,
+//     endereco:endereco,
+//     senha:"senha123"
+//  }
+// usuario.nome = "jose"
+//  console.log(usuario.nome, usuario.idade, usuario.senha, usuario.endereco);
+//  console.log(usuario);
+ 
+// const id = prompt("digete o id do produto: ");
+// const nome = prompt("digete o nome do produto: ");
+// const preco = Number(prompt("digite o preço do protudo"))
+
+// let produtos=[]
+// function adicionarproduto(id,nome,preco){
+//     const produto ={
+//         idproduto:id,
+//         nomeproduto:nome,
+//         precoproduto:preco
+//     }
+//     produtos.push(produto)
+// }
+// adicionarproduto(id,nome,preco)
+// console.log(produtos)
+
+// const filme = {
+// 	nome: "Auto da Compadecida", 
+// 	ano: 2000, 
+// 	elenco: [
+// 		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+// 		"Virginia Cavendish"
+// 		], 
+// 	transmissoesHoje: [
+// 		{canal: "Telecine", horario: "21h"}, 
+// 		{canal: "Canal Brasil", horario: "19h"}, 
+// 		{canal: "Globo", horario: "14h"}
+// 		]
+// }
+
+// console.log(filme.elenco[0])
+// console.log(filme.elenco[filme.elenco.length - 1])
+// console.log(filme.transmissoesHoje[2])
+// const cachorro = {
+// 	nome: "Juca", 
+// 	idade: 3, 
+// 	raca: "SRD"
+// }
+
+// const gato = {...cachorro, nome: "Juba"}
+
+// const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+// console.log(cachorro)
+// console.log(gato)
+// console.log(tartaruga)
+
+// function minhaFuncao(objeto, propriedade) {
+// 	return objeto[propriedade]
+// }
+
+// const pessoa = {
+//   nome: "Caio", 
+//   idade: 23, 
+//   backender: false
+// }
+
+// console.log(minhaFuncao(pessoa, "backender"))
+// console.log(minhaFuncao(pessoa, "altura"))
+// Exemplo de entrada
+// const pessoa = {
+//     nome: "joão", 
+//     apelidos: ["jao", "joaozinho", "jo"]
+// }
+// console.log(pessoa)
+
+// const pessoa = {
+//     nome: "paulo",
+//     idade:50
+// } 
+
+// const novapessoa = {
+//     ...pessoa
+// }
+// console.log(novapessoa)
+
+// const pessoa = {
+// 	nome: "joao", 
+//   idade: 15, 
+// 	profissao: "prostituta"
+// }
+
+// function minhaFuncao(pessoa) {
+//   const resultado = []
+//   resultado.push(pessoa.nome,pessoa.idade,pessoa.profissao)
+
+//   return resultado
+// }
+
+// console.log(minhaFuncao(pessoa))
+
+ 
+// let carrinho = []
+// banana = {
+//     nome: "banana",
+//     disponibilidade: true
+// }
+// pitaia = {
+//     nome: "pitaia",
+//     disponibilidade: true
+// }
+// manga = {
+//     nome: "manga",
+//     disponibilidade: true
+// }
+
+// function pegarFuta (objeto) {
+//     carrinho.push(objeto)
+// }
+// pegarFuta(banana)
+// pegarFuta(pitaia)
+// pegarFuta(manga)
+
+// console.log(carrinho)
+
+
+
+
+
